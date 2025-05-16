71cde932a93a1ca2282c68ce8ccef229b757d603
Siavash Mirzaei-Fard <siavash.mf@gmail.com>
2025-05-16 08:11:10 +0200

    Refactor SplashScreen
    Add a new parameter to scale icon size, in previous implementation animation did not make sence!

--- app/src/main/java/com/cyafard/budgettracker/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/cyafard/budgettracker/presentation/screen/splash/SplashScreen.kt
@@ -1,22 +1,28 @@
 package com.cyafard.budgettracker.presentation.screen.splash
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.EaseInBounce
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import kotlinx.coroutines.flow.collectLatest
@@ -27,7 +33,11 @@ fun SplashScreen(
     navController: NavController,
     viewModel: SplashViewModel = hiltViewModel()
 ) {
-    var visible by remember { mutableStateOf(false) }
+    var visible by rememberSaveable { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = 1f,
+        animationSpec = tween(durationMillis = 1000, easing = EaseInBounce)
+    )
 
     LaunchedEffect(true) {
         visible = true
@@ -53,7 +63,8 @@ fun SplashScreen(
         ) {
             Icon(
                 imageVector = Icons.Default.CheckCircle,
-                contentDescription = null
+                contentDescription = null,
+                modifier = modifier.size(120.dp).scale(scale)
             )
         }
     }
